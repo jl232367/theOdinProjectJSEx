d4be46bec8cf6b27348c854017f396bb91221d72
Jacob Luallen <jluallen@fcc-jc.org>
2021-04-06 19:01:12 -0400

    solved palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function() {
+const palindromes = function(pal) {
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    const removePunct = pal.replace(regex, "");
+    const removeSpace = removePunct.split(" ").join("");
+    const reversePal = reverseStr(removeSpace);
+    removeSpace.toLowerCase();
+    reversePal.toLowerCase();
 
+    if (removeSpace.toLowerCase() === reversePal.toLowerCase()) {
+        return true;
+    } else { return false;}
+
+    function reverseStr(oldStr) {
+        let newStr = "";
+        for (let i = oldStr.length - 1; i >= 0; i--) { 
+            newStr += oldStr[i]; // or newString = newString + str[i];
+        }
+        return newStr; 
+    }
 }
 
 module.exports = palindromes
