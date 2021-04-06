91f42905fb994a1fa8326fa9b90de116c3c0ed0c
Jacob Luallen <jluallen@fcc-jc.org>
2021-04-06 19:58:33 -0400

    working on fibonacci, not getting very far

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num <= 0) {
+        return "OOPS";
+    } else {
+        let third = 1;
+        let second = 1;
+        let first = 0;
+        for (let i = 0; i < num; i++){
+            let firstAdd = first + second;
+            let secondAdd = second + third;
+            let start = firstAdd + secondAdd;
+            
+        }
 }
-
+}
 module.exports = fibonacci
